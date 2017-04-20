1e6186fd223778b1fcdcd23fb2f4511d0114ed38
Kirill <gkarnd@yandex.ru>
2017-04-20 22:41:43 +0300

    Добавьте файлы проекта.

---
+++ Assets/CubeDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[RequireComponent(typeof(Collider))]
+public class CubeDestroy : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Stop()
+    {
+        Destroy(gameObject);
+    }
+    public void Destr()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        Destroy(gameObject, 0.75f);
+    }
+}
---
+++ Assets/cubeCreater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeCreater : MonoBehaviour {
+
+    public GameObject cube;
+    int count = 0;
+	
+	void Start ()
+    { 
+       StartCoroutine(instCube());  	
+	}
+	IEnumerator instCube()
+    {
+        while (count < 20)
+        {
+            yield return new WaitForSeconds(2f);
+            Instantiate(cube, new Vector3(Random.Range(-9f, -3f), Random.Range(0f, 2f), Random.Range(-12f, -6f)), Quaternion.identity);
+            count++;
+        }
+    }
+	void Update ()
+    {
+        
+	}
+    
+}
